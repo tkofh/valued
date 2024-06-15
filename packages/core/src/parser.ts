3df2a5caf556c82b20afcc0e7d0761195810a952
Tim Morris <timthewebguy@gmail.com>
2024-06-15 15:30:34 -0400

    remove old code

--- packages/core/src/parser.ts
+++ packages/core/src/parser.ts
@@ -1,17 +1,7 @@
 import type { Token } from './tokenizer'
 
-// export type ParserShapeMeta =
-//   | { finite: false }
-//   | { finite: true; length: number }
-//
-// export type ParserShape = IterableIterator<ReadonlySet<string>> &
-//   ParserShapeMeta
-
 export interface Parser<T> {
   readonly isSatisfied: boolean
-  // readonly isFull: boolean
-
-  // shape(): ParserShape
   feed(token: Token): boolean
   flush(): T | undefined
   reset(): void
@@ -36,62 +26,3 @@ const _invalid: ParseResult<never> = { valid: false }
 export function invalid<T>(): ParseResult<T> {
   return _invalid
 }
-
-// const validStatus = 1
-// const fullStatus = 2
-// const consumedStatus = 4
-//
-// export const status = {
-//   none: 0,
-//   valid: validStatus,
-//   full: fullStatus,
-//   consumed: consumedStatus,
-//   all: validStatus | fullStatus | consumedStatus,
-// } as const
-//
-// function* walkParserShapes(
-//   shapes: ReadonlySet<ParserShape>,
-// ): IterableIterator<ReadonlySet<string>> {
-//   const values = new Set<string>()
-//   while (true) {
-//     values.clear()
-//
-//     for (const shape of shapes) {
-//       const shapeValues = shape.next().value as ReadonlySet<string>
-//       for (const value of shapeValues) {
-//         values.add(value)
-//       }
-//     }
-//
-//     yield values
-//   }
-// }
-//
-// export function mergeParserShapes(
-//   parsers: ReadonlyArray<Parser<unknown>>,
-//   lengthMode: 'sum' | 'max' = 'sum',
-// ): ParserShape {
-//   let length = 0
-//   let finite = true
-//
-//   const shapes = new Set<ParserShape>()
-//
-//   for (const parser of parsers) {
-//     const shape = parser.shape()
-//     shapes.add(shape)
-//
-//     if (!shape.finite) {
-//       finite = false
-//     } else if (finite) {
-//       length =
-//         lengthMode === 'max'
-//           ? Math.max(length, shape.length)
-//           : length + shape.length
-//     }
-//   }
-//
-//   return Object.assign(
-//     walkParserShapes(shapes),
-//     (finite ? { finite: true, length } : { finite: false }) as ParserShapeMeta,
-//   )
-// }
